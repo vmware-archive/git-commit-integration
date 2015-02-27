9297acb1cf2766936609a6b3fd77d7552a88e90e
joellen <joellen.carter@gmail.com>
2015-02-27 15:50:14 -0700

    [#89278622] fix failing spec

--- spec/views/home/show.html.erb_spec.rb
+++ spec/views/home/show.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "home/show.html.erb" do
   it "renders title" do
     render
-    expect(rendered).to match("HOME PAGE")
+    expect(rendered).to match(/Project Backlog/)
   end
 end
