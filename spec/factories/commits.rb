d0037f529e91e9a1afddf87b110b0b015fc62f1f
Chad Woolley <thewoolleyman@gmail.com>
2015-05-20 16:13:40 -0700

    remove old reference in factory

--- spec/factories/commits.rb
+++ spec/factories/commits.rb
@@ -2,8 +2,7 @@ FactoryGirl.define do
   factory :commit do
     data "MyString"
 sha "MyString"
-parent_sha "MyString"
-    patch_identifier "MyString"
+patch_identifier "MyString"
 message "MyString"
 author_github_user_id 1
 author_date "2015-02-27 15:44:27"
