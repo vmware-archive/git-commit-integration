0146166743f131b473779d0d3c088a4a8a7baf5d
Chad Woolley <thewoolleyman@gmail.com>
2015-05-21 21:56:55 -0700

    [#95216938] Do not show merge commits duplicated when listing all commits for a repo

--- app/models/parent_commit.rb
+++ app/models/parent_commit.rb
@@ -3,6 +3,7 @@ class ParentCommit < ActiveRecord::Base
   belongs_to :child_commit, :class_name => 'Commit', :foreign_key => 'child_commit_id'
 
   validates_presence_of :sha
+  validates_presence_of :commit_id
   validates_presence_of :child_commit_id
   validates_uniqueness_of :sha, scope: :child_commit_id
   validates_uniqueness_of :commit_id, scope: :child_commit_id, allow_nil: true
--- lib/ordered_commits.rb
+++ lib/ordered_commits.rb
@@ -4,8 +4,9 @@ module OrderedCommits
     # TODO: How to use arel for left join?  What docs say doesn't work:
     # .join(ParentCommit.arel_table, Arel::Nodes::OuterJoin)
     unordered_commits.
-      joins('LEFT JOIN parent_commits ON parent_commits.child_commit_id = commits.id').
-      reorder('committer_date ASC, child_secondary_sort_order DESC')#.
-      # select('parent_commits.commit_id AS parent_commit_id')
+      joins('LEFT JOIN parent_commits ON (parent_commits.child_commit_id = commits.id)').
+      reorder('committer_date ASC, parent_commits.child_secondary_sort_order DESC').
+      select('commits.*, parent_commits.child_secondary_sort_order').
+      distinct('commits.id')
   end
 end
