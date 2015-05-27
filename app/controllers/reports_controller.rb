12993c41cd556991a5dfd673e12d7bd988d59f07
Chad Woolley <thewoolleyman@gmail.com>
2015-05-26 17:48:51 -0700

    remove useless placeholder external links report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,4 @@
 class ReportsController < ApplicationController
-  def external_links
-    @external_links = ExternalLink.all
-  end
-
   def external_link_ref_commits
     @external_link = ExternalLink.find(params.fetch(:external_link_id))
   end
